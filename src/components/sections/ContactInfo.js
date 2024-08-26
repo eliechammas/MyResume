87a75c736973fd9d8c65fae28cb273ddb5d519b0
Elie Chammas <contact@eliechammas.me>
2024-08-26 10:45:04 +0200

    commit

--- src/components/sections/ContactInfo.js
+++ src/components/sections/ContactInfo.js
@@ -7,12 +7,12 @@ const ContactInfo = () => {
       <div className="row">
         <div className="col col-d-12 col-t-12 col-m-12 border-line-v">
           <div className="map">
-            <iframe
-              src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39149.023526614794!2d9.92681913881927!3d52.174075671972595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47baaf1f4f4e74c5%3A0x744622d890779db0!2sG%C3%BCldenfeld%2077%2C%2031135%20Hildesheim!5e0!3m2!1sen!2sde!4v1724661234423!5m2!1sen!2sde"
-              style={{ border: 0, width: "100%", height: "100%" }}
-              allowFullScreen=""
-              loading="lazy"
-            />
+<iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39149.023526614794!2d9.92681913881927!3d52.174075671972595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47baaf1f4f4e74c5%3A0x744622d890779db0!2sG%C3%BCldenfeld%2077%2C%2031135%20Hildesheim!5e0!3m2!1sen!2sde!4v1724661234423!5m2!1sen!2sde"
+    style={{ border: 0, width: "100%", height: "100%" }}
+    allowFullScreen=""
+    loading="lazy"
+/>
           </div>
           <div className="info-list">
             <ul>
