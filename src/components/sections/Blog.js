8f408ac3a06123a8635023c55efa91e86d487a44
Elie Chammas <contact@eliechammas.me>
2024-09-23 00:33:51 +0200

    adding test blog

--- src/components/sections/Blog.js
+++ src/components/sections/Blog.js
@@ -40,17 +40,7 @@ const BlogSection = () => {
                 <div className="text">
                   <p>
                     Programming Language: SQL Server
-                  </p>
-                  <p>
-                    You are given two tables, teams and matches, with the following structures:
-                    Each record in the table teams represents a single soccer team. Each record in the table matches represents a finished match between two teams. Teams (host_team, guest_team) are represented by their IDs in the teams table (team_id). No team plays a match against itself. You know the result of each match (that is, the number of goals scored by each team).
-
-                    You would like to compute the total number of points each team has scored after all the matches described in the table. The scoring rules are as follows:
-
-                    If a team wins a match (scores strictly more goals than the other team), it receives three points.
-                    If a team draws a match (scores exactly the same number of goals as the opponent), it receives one point.
-                    If a team loses a match (scores fewer goals than the opponent), it receives no points.
-                    Write an SQL query that returns a ranking of all teams (team_id) described in the table teams. For each team you should provide its name and the number of points it received after all described matches (num_points). The table should be ordered by num_points (in decreasing order). In case of a tie, order the rows by team_id (in increasing order).
+                    You are given two tables, teams and matches, with the following structures:...
                   </p>
                 </div>
               </div>
