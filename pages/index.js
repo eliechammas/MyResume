0dfa5381dbbc2aacd82b4d7098c24be4e06521aa
Elie Chammas <contact@eliechammas.me>
2025-03-20 10:15:13 +0200

    enhancements 1

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ const RecentWorks = dynamic(
 
 
 const bio = `<p>
-I am a software developer living in Lebanon  <br /><br />
+I am a software developer living in Lebanon.  <br /><br />
 I have a wide expertise in translating business requirements into technical solutions ensuring quality,
 usability, security, and scalability with more than 15+ years of professional experience. <br /><br />
 I speak english, french and arabic fluently. <br />
