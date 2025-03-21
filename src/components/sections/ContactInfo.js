6256922a0e5a0b3d18b865c549c6b9174859aa70
Elie Chammas <contact@eliechammas.me>
2025-03-21 08:39:40 +0200

    Google map adjustment

--- src/components/sections/ContactInfo.js
+++ src/components/sections/ContactInfo.js
@@ -8,7 +8,7 @@ const ContactInfo = () => {
         <div className="col col-d-12 col-t-12 col-m-12 border-line-v">
           <div className="map">
 <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3315.2956002506195!2d35.49699377554117!3d33.888628373230236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f1700381b5d0b%3A0x9d98e88d6c0958d9!2sBeirut!5e0!3m2!1sen!2slb!4v1724661234423!5m2!1sen!2slb"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3314.459457163625!2d35.54247107554208!3d33.88897347312637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f3ddc35a0420f%3A0x8b3d6caa39092f34!"
     style={{ border: 0, width: "100%", height: "100%" }}
     allowFullScreen=""
     loading="lazy"
